1bf953208b83d4da389c06d8d146949337fd56d7
David Cruwys <david@ideasmen.com.au>
2024-11-25 13:44:24 +1000

    6-add top level pages for about, authentication & account

---
+++ a.sh
@@ -0,0 +1,4 @@
+alias rs='rails server -p 3001'
+alias gac='git add . && git commit -m '
+alias r8_next="rails app:template LOCATION='http://localhost:3001/template/rails8.0.rb'"
+alias run="bin/rails assets:precompile && rs"
